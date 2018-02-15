5c827512863232719eb5776af9bf27e56ac68e94
NegatioN <joakimrishaug@gmail.com>
2018-02-15 07:48:44 +0100

    option to persist workout in a json-file

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import json
 from datetime import date
 
 PORT = 8080
+SAVE_LOCATION = 'my_workouts.json'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,7 +20,7 @@ input_parser = Parser(exercise_date=date.today(),
 
 #TODO collapse multiple inputs of the same exercise to ONE. ex: s,1x2;s,1x2;s,1x3
 
-async def handle(request):
+async def json_workout(request):
     workout = request.match_info.get('workout', "nothing")
     try:
         workout_data = input_parser.parse_user_input(workout)
@@ -28,8 +29,32 @@ async def handle(request):
         logging.WARN("Got unprocessable input: {}".format(workout))
         return web.Response(status=422)
 
+def concat_content(workout, prev_workouts):
+    if len(prev_workouts) > 0:
+        prev_workouts.append(workout)
+    else:
+        return [workout]
+
+async def persist_workout(request):
+    workout = request.match_info.get('workout', "nothing")
+    try:
+        workout_data = input_parser.parse_user_input(workout)
+        print(workout_data)
+        with open(SAVE_LOCATION, 'w+') as f:
+            if f.read() == '':
+                prev_content = []
+            else:
+                prev_content = json.loads(f)
+            prev_content.append(workout_data)
+            json.dump(prev_content, f)
+        return web.Response(text='Added workout')
+    except Exception as e:
+        logging.WARN("Got unprocessable input: {}".format(workout))
+        return web.Response(status=422)
+
 app = web.Application()
-app.router.add_get('/workout/{workout}', handle)
+app.router.add_get('/workout/{workout}', json_workout)
+app.router.add_get('/workout/save/{workout}', persist_workout)
 
 if __name__ == "__main__":
     web.run_app(app, port=PORT)
