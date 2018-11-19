66ad72b17c179b62933f54cf2c7c78ccd96cac3e
NegatioN <joakimrishaug@gmail.com>
2018-11-19 20:52:29 +0100

    Move reading of secret to a more correct place

--- reload.sh
+++ reload.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-. /home/joakim/workout_secret
 git pull --rebase
 sudo pip3 install -r requirements.txt
-sudo systemctl import-environment WORKOUT_SECRET
 sudo systemctl restart worklog.service
